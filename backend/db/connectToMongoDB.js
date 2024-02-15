bd8855bfaeeb9091dcfb6de79581e5613be0c866
ponnanapavan <pavanponnana1@gmail.com>
2024-02-15 06:31:42 +0530

    changes

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URL);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+    try {
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        await mongoose.connect(process.env.MONGO_URL, options);
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error.message);
+    }
 };
 
 export default connectToMongoDB;
